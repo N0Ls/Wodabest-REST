52e03336256af4f49bc474664146fcfaaeee5259
vscav <vscavinner@gmail.com>
2020-05-17 22:51:55 +0200

    Clean upload directory on rollback

--- database/migrations/2020_05_04_222406_create_images_table.php
+++ database/migrations/2020_05_04_222406_create_images_table.php
@@ -7,6 +7,29 @@ use Illuminate\Support\Facades\Schema;
 class CreateImagesTable extends Migration
 {
     /**
+     * Clean a directory.
+     * 
+     * @param String $dirname
+     * @return Boolean
+     */
+    private function cleanDirectory($dirname) {
+        if (is_dir($dirname))
+           $dir_handle = opendir($dirname);
+        if (!$dir_handle)
+          return false;
+        while($file = readdir($dir_handle))
+        {
+           if ($file != "." && $file != "..")
+           {
+                if (!is_dir($dirname."/".$file))
+                     unlink($dirname."/".$file);
+           }
+        }
+        closedir($dir_handle);
+        return true;
+    }
+
+    /**
      * Run the migrations.
      *
      * @return void
@@ -41,6 +64,7 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
+        Self::cleanDirectory('public/img/upload');
         Schema::dropIfExists('images');
     }
 }
